91f06940737f4a568d2162f269d7f81ebb93c1c1
abhinav0515 <AbhinavPandey@atmc.edu.au>
2025-09-08 21:06:06 +0530

    dekho ab kab hoga

--- app/components/LearningHighlightsSection.tsx
+++ app/components/LearningHighlightsSection.tsx
@@ -48,13 +48,13 @@ const LearningHighlightsSection = () => {
             {highlights.map((highlight, index) => (
               <div
                 key={index}
-                className="flex-shrink-0 w-72 bg-black rounded-lg border border-gray-200 p-2 lg:p-4"
+                className="flex-shrink-0 w-72 bg-gray-50 rounded-lg  p-2 lg:p-4"
               >
-                <h3 className=" text-md lg:text-lg font-semibold mb-3 text-white font-section-heading">
+                <h3 className=" text-md lg:text-lg font-semibold mb-3 text-black font-section-heading">
                   {highlight.title}
-                  <hr />
+         
                 </h3>
-                <p className="text-xs lg:text-sm text-white leading-relaxed">
+                <p className="text-xs lg:text-sm text-black leading-relaxed">
                   {highlight.description}
                 </p>
               </div>
